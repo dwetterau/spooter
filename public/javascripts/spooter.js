be030958425c78b98291997dba7843e71410bfd2
jmslocum16 <jmslocum16@yahoo.com>
2015-05-14 21:50:14 -0500

    added client stuff

---
+++ public/javascripts/spooter.js
@@ -0,0 +1,95 @@
+// TODO remove
+window.spooter = { state:
+{
+  playerId: 0,
+  worldWidth: 640,
+  worldHeight: 480,
+  entities: [
+    { x: 100,
+      y: 100,
+      vx: 10,
+      vy: 10,
+      r: 20,
+      type: "player",
+      id: 0
+    },
+    { x: 20,
+      y: 200,
+      vx: 30,
+      vy: 0,
+      r: 6,
+      type: "bullet",
+      id: 1
+    },
+    { x: 400,
+      y: 400,
+      vx: 0,
+      vy: 0,
+      r: 15,
+      type: "enemy",
+      id: 2
+    },
+    { x: 300,
+      y: 200,
+      vx: 0,
+      vy: 15,
+      r: 20,
+      type: "player",
+      id: 3
+    }
+  ]
+}};
+
+var lastms = 0;
+
+function gameLoop() {
+  var curms = new Date().getTime();
+  if (lastms == 0) {
+    lastms = curms;
+  }
+  var msdif = curms - lastms;
+  lastms = curms;
+  update(msdif);
+  draw();
+  setTimeout(gameLoop, 1);
+}
+
+// basically move objects along their velocity
+function update(framems) {
+  var updateState = window.spooter.state;
+  for (var i = 0; i < updateState.entities.length; i++) {
+    updateState.entities[i].x += updateState.entities[i].vx * framems/1000.0;
+    updateState.entities[i].y += updateState.entities[i].vy * framems/1000.0;
+  }
+}
+
+var c=document.getElementById("spooterCanvas");
+var ctx=c.getContext("2d"); 
+
+function drawCircle(x, y, radius) {
+  ctx.beginPath();
+  ctx.arc(x, y, radius, 0, 2*Math.PI);
+  ctx.fill();
+}
+
+function draw() {
+  ctx.fillStyle = "#FFFFFF";
+  ctx.fillRect(0, 0, 640, 640);
+  var drawState = window.spooter.state;
+  for (var i = 0; i < drawState.entities.length; i++) {
+    if (drawState.entities[i].type === "player") {
+      if (drawState.entities[i].id == window.spooter.state.playerId) {
+        ctx.fillStyle = "#FF0000";
+      } else {
+        ctx.fillStyle = "#FFFF00";
+      }
+    } else if (drawState.entities[i].type === "enemy") {
+      ctx.fillStyle = "#5E2D79";
+    } else {
+      ctx.fillStyle = "#CC8800";
+    }
+    drawCircle(drawState.entities[i].x, drawState.entities[i].y, drawState.entities[i].r);
+  }
+}
+
+gameLoop();
