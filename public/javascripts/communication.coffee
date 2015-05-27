2563521e4f4cfaa4fdf1bcc625d7f638a51c77b5
David Wetterau <david.wetterau@gmail.com>
2015-05-27 14:57:23 -0500

    better interpolation

--- public/javascripts/communication.coffee
+++ public/javascripts/communication.coffee
@@ -2,16 +2,59 @@
 socket = io('http://localhost:3001') # TODO change localhost to server endpoint
 
 socket.on 'initialize', (data) ->
+  console.log "initializing..."
   initialize(data)
 
 serializer = null
 
 socket.on 'state', (data) ->
   if serializer
-    setState serializer.toObject(data, window.spooter.state.newState)
+    setState serializer.toObject(data, nextState(true))
   else
     if window.spooter.StateSerializer?
       serializer = new window.spooter.StateSerializer()
+
+_setStateAtIndex = (index, object) ->
+  window.spooter.states[index] = object
+
+_getStateAtIndex = (index) ->
+  state = window.spooter.states[index]
+  if not state?
+    return null
+  return state
+
+nextState = (create) ->
+  index = (window.spooter.stateBufferIndex + 1) % window.spooter.STATE_BUFFER_SIZE
+  state = _getStateAtIndex index
+  if not state
+    _setStateAtIndex index, {}
+    return _getStateAtIndex index
+  return state
+
+# Returns the states to interpolate between with the time
+getStatesToInterpolate = (time) ->
+  leftIndex = null
+  # Find the states that this time lies between
+  for t, index in window.spooter.stateTimes
+    if t <= time and window.spooter.stateTimes[(index + 1) % window.spooter.STATE_BUFFER_SIZE] >= time
+      leftIndex = index
+      break
+
+  # If we didn't find some enclosing states, exit
+  if not leftIndex
+    return null
+
+  rightIndex = (leftIndex + 1) % window.spooter.STATE_BUFFER_SIZE
+  return {
+    leftState: _getStateAtIndex leftIndex
+    rightState: _getStateAtIndex rightIndex
+    leftTime: window.spooter.stateTimes[leftIndex]
+    rightTime: window.spooter.stateTimes[rightIndex]
+  }
+
+incrStateBufferIndex = () ->
+  window.spooter.stateBufferIndex++
+  window.spooter.stateBufferIndex %= window.spooter.STATE_BUFFER_SIZE
 
 move = (ax, ay) ->
   playerId = getPlayerId()
@@ -37,20 +80,26 @@ getPlayerId = ->
 
 initialize = (data) ->
   {worldHeight, worldWidth, playerId} = data
-  window.spooter.state = {
-    drawState: {}
-    newState: {}
-  }
+  window.spooter.STATE_BUFFER_SIZE = 50;
+  window.spooter.states = new Array(window.spooter.STATE_BUFFER_SIZE)
+  window.spooter.stateTimes = new Array(window.spooter.STATE_BUFFER_SIZE)
+  window.spooter.stateBufferIndex = -1
+  window.spooter.stateStartTime = null
+
   window.spooter.playerId = playerId
   window.spooter.worldHeight = worldHeight
   window.spooter.worldWidth = worldWidth
 
+  window.spooter.move = move
+  window.spooter.shoot = shoot
+  window.spooter.getStatesToInterpolate = getStatesToInterpolate
   window.spooter.initialized = false
 
 lastStateReceivedTime = 0
 totalStateTime = 0
 numStateUpdates = 0.0
 setState = (newState) ->
+  # Collect data on how long between updates.
   now = new Date().getTime()
   if lastStateReceivedTime != 0
     time = now - lastStateReceivedTime
@@ -60,14 +109,15 @@ setState = (newState) ->
       console.log "Average state update interval", (totalStateTime / numStateUpdates)
   lastStateReceivedTime = now
 
-  # Swap the state pointers
-  oldState = window.spooter.state.drawState
-  window.spooter.state.drawState = newState
-  window.spooter.state.newState = oldState
+  incrStateBufferIndex()
+
+  now = new Date().getTime()
+  if window.spooter.stateStartTime is null
+    window.spooter.stateStartTime = now
+
+  window.spooter.stateTimes[window.spooter.stateBufferIndex] = (
+    now - window.spooter.stateStartTime)
 
   window.spooter.initialized = true
 
-window.spooter = {initialized: false, move, shoot, state: {
-  drawState: {}
-  newState: {}
-}}
+window.spooter = {}
--- public/javascripts/spooter.js
+++ public/javascripts/spooter.js
@@ -14,6 +14,9 @@ var lastAx = 0;
 var lastAy = 0;
 var clicked;
 
+// The amount in milliseconds clients are forced to be behind
+var FORCED_DELAY = 50;
+
 window.addEventListener('resize', resizeCanvas, false);
 
 function resizeCanvas() {
@@ -34,16 +37,39 @@ canvas.addEventListener('click', function(evt) {
   window.spooter.shoot();
 }, false);
 
-function pollMouse() {
-  if (mouseX >= 0 && mouseY >= 0 && window.spooter.state.drawState) {
-    p = undefined;
-    for (i = 0; i < window.spooter.state.drawState.numEntities; i++) {
-      entity = window.spooter.state.drawState.entities[i];
-      if (window.spooter.playerId == entity.id && entity.type == "player") {
-        p = window.spooter.state.drawState.entities[i];
-        break;
-      }
+function getState() {
+  if (interpolatedState) {
+      return interpolatedState;
+  }
+  if (window.spooter.states == undefined ||
+      window.spooter.stateBufferIndex == undefined) {
+    return null;
+  }
+  var index = window.spooter.stateBufferIndex;
+  if (window.spooter.states[index] != undefined) {
+    return window.spooter.states[index]
+  }
+  return null;
+}
+
+function getPlayer() {
+  var state = getState();
+  if (!state) {
+    return null;
+  }
+  var i = 0;
+  for (i = 0; i < state.numEntities; i++) {
+    var entity = state.entities[i];
+    if (window.spooter.playerId == entity.id && entity.type == 'player') {
+      return entity;
     }
+  }
+  return null;
+}
+
+function pollMouse() {
+  if (mouseX >= 0 && mouseY >= 0) {
+    var p = getPlayer();
     if (p) {
       ax = viewportX + mouseX - p.x;
       ay = viewportY + mouseY - p.y;
@@ -53,43 +79,80 @@ function pollMouse() {
         lastAy = ay;
         window.spooter.move(ax, ay);
       }
-      //window.spooter.move(viewportX + mouseX, viewportY + mouseY);
     }
   }
   setTimeout(pollMouse, 10);
 }
 
 // updating state
-totalMSDif = 0.0;
-numMSDif = 0;
+var totalMSDif = 0.0;
+var numMSDif = 0;
+var drawStartTime = null;
+var interpolatedState = null;
 function gameLoop() {
-  var curms = new Date().getTime();
-  if (lastms == 0) {
-    lastms = curms;
+  var now = new Date().getTime();
+  if (!drawStartTime && window.spooter.stateStartTime) {
+    drawStartTime = window.spooter.stateStartTime + FORCED_DELAY;
   }
-  var msdif = curms - lastms;
-  totalMSDif += msdif;
+
+  // Compute average frame update timing information
+  if (lastms == 0) {
+    lastms = now;
+  }
+  totalMSDif += now - lastms;
   numMSDif++;
   if (Math.random() < .0001) {
       console.log("Average Time between frames", (totalMSDif / numMSDif));
   }
 
-  lastms = curms;
+  lastms = now;
 
   if (window.spooter.initialized) {
-    update(msdif);
-    draw();
+    if (update(now - drawStartTime)) {
+      draw(interpolatedState);
+    }
   }
   setTimeout(gameLoop, 1);
 }
 
-// basically move objects along their velocity
-function update(framems) {
-  var updateState = window.spooter.state.drawState;
-  for (var i = 0; i < updateState.numEntities; i++) {
-    updateState.entities[i].x += updateState.entities[i].vx * framems/1000.0;
-    updateState.entities[i].y += updateState.entities[i].vy * framems/1000.0;
+stop = false;
+// Interpolate the positions of each entity between the two given states
+function update(drawTime) {
+  object = window.spooter.getStatesToInterpolate(drawTime);
+  if (object == null) {
+    return false;
   }
+  var t0 = object.leftTime;
+  var t1 = object.rightTime;
+  var leftEntityMap = {};
+  for (var i = 0; i < object.leftState.numEntities; i++) {
+    var entity = object.leftState.entities[i];
+    leftEntityMap[entity.id + entity.type] = object.leftState.entities[i]
+  }
+  interpolatedState = {numEntities: object.rightState.numEntities};
+  interpolatedState.entities = new Array(object.rightState.numEntities);
+  for (i = 0; i < object.rightState.numEntities; i++) {
+    entity = object.rightState.entities[i];
+    var newEntity = {
+      type: entity.type,
+      r: entity.r,
+      id: entity.id
+    };
+    if (leftEntityMap.hasOwnProperty(entity.id + entity.type)) {
+      var leftEntity = leftEntityMap[entity.id + entity.type];
+      // An updated entity, interpolate between the two states
+      var x0 = leftEntity.x;
+      var y0 = leftEntity.y;
+      var x1 = entity.x;
+      var y1 = entity.y;
+      newEntity.x = (x1 - x0) / (t1 - t0) * (drawTime - t0) + x0;
+      newEntity.y = (y1 - y0) / (t1 - t0) * (drawTime - t0) + y0;
+      if (entity.id == 0) {
+      }
+    }
+    interpolatedState.entities[i] = newEntity;
+  }
+  return true;
 }
 
 // drawing stuff
@@ -119,13 +182,12 @@ function drawLine(x1, y1, x2, y2) {
   ctx.lineTo(x2, y2);
 }
 
-function draw() {
+function draw(state) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
-  var drawState = window.spooter.state.drawState;
-  for (var i = 0; i < drawState.numEntities; i++) {
-    if (drawState.entities[i].type === "player" && drawState.entities[i].id == window.spooter.playerId) {
-      viewportX = drawState.entities[i].x - (canvas.width >> 1);
-      viewportY = drawState.entities[i].y - (canvas.height >> 1);
+  for (var i = 0; i < state.numEntities; i++) {
+    if (state.entities[i].type === "player" && state.entities[i].id == window.spooter.playerId) {
+      viewportX = state.entities[i].x - (canvas.width >> 1);
+      viewportY = state.entities[i].y - (canvas.height >> 1);
       break;
     }
   }
@@ -147,20 +209,20 @@ function draw() {
   endDrawLine();
 
   // draw entities
-  for (i = 0; i < drawState.numEntities; i++) {
-    if (!inViewport(drawState.entities[i])) continue;
-    if (drawState.entities[i].type === "player") {
-      if (drawState.entities[i].id == window.spooter.playerId) {
+  for (i = 0; i < state.numEntities; i++) {
+    if (!inViewport(state.entities[i])) continue;
+    if (state.entities[i].type === "player") {
+      if (state.entities[i].id == window.spooter.playerId) {
         ctx.fillStyle = "#FF0000";
       } else {
         ctx.fillStyle = "#FFFF00";
       }
-    } else if (drawState.entities[i].type === "enemy") {
+    } else if (state.entities[i].type === "enemy") {
       ctx.fillStyle = "#5E2D79";
     } else {
       ctx.fillStyle = "#CC8800";
     }
-    drawCircle(drawState.entities[i].x - viewportX, drawState.entities[i].y - viewportY, drawState.entities[i].r);
+    drawCircle(state.entities[i].x - viewportX, state.entities[i].y - viewportY, state.entities[i].r);
   }
 }
 
